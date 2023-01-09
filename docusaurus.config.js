cdb3a3497deaf68b07f83008e47a545291276e7e
Sivert <sivertgh@stud.ntnu.no>
2023-01-09 17:02:58 +0100

    updated 'edit link'

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,14 +37,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/Gryta-Krutt/docs',
+            'https://github.com/Gryta-Krutt/docs/tree/main',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/Gryta-Krutt/docs',
+            'https://github.com/Gryta-Krutt/docs/tree/main',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
