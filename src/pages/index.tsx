2e3ee04c50c5a39eaee5f9f41a2bdc14683f7ac5
Sivert <sivertgh@stud.ntnu.no>
2023-01-09 17:44:13 +0100

    updated og image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ import Head from '@docusaurus/Head';
 export default function Home() {
   const description =
     "Documentation for Gryt - The open source chat platform";
-  const imagePreview = '/img/meta/index.png';
+  const imagePreview = '/img/meta/Gryt.chat.og.banner.png';
   const site = 'https://gryt-docs.vercel.app/';
   return (
     <Layout
