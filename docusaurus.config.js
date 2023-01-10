6e0ccb0f603e2ef0c10f9b27800c0d9d16bdde30
Sivert <sivertgh@stud.ntnu.no>
2023-01-10 17:59:24 +0100

    Updated github org name

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'Gryta-Krutt', // Usually your GitHub org/user name.
+  organizationName: 'Gryt-chat', // Usually your GitHub org/user name.
   projectName: 'docs', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
@@ -37,14 +37,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/Gryta-Krutt/docs/tree/main',
+            'https://github.com/Gryt-chat/docs/tree/main',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/Gryta-Krutt/docs/tree/main',
+            'https://github.com/Gryt-chat/docs/tree/main',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
