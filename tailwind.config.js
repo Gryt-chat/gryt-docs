b09fc827d58fae4262a1f4539cfe9638b34563f3
Sivert <sivertgh@stud.ntnu.no>
2023-01-11 20:27:40 +0100

    Updated styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,23 +4,35 @@ module.exports = {
     preflight: false, // disable Tailwind's reset
   },
   content: ["./src/**/*.{js,jsx,ts,tsx}", "../docs/**/*.mdx"], // my markdown stuff is in ../docs, not /src
-  darkMode: ['class', '[data-theme="dark"]'], // hooks into docusaurus' dark mode settigns
+  darkMode: ["class", '[data-theme="dark"]'], // hooks into docusaurus' dark mode settigns
   theme: {
     extend: {},
   },
-  safelist: [
-    'daisy-bordered', 'daisy-bg-base-100', 'w-full', 'rounded-2xl'
-],
+  safelist: ["w*", "h*", "rounded*", "text*", "daisy-*", "bg*"],
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
-  
+
   // daisyUI config (optional)
   daisyui: {
     styled: true,
-    themes: ['dracula'],
+    themes: [
+      {
+        grytDocs: {
+          primary: "#818cf8",
+          secondary: "#BF95F9",
+          accent: "#FFB86B",
+          neutral: "#414558",
+          "base-100": "#272935",
+          info: "#8BE8FD",
+          success: "#52FA7C",
+          warning: "#F1FA89",
+          error: "#FF5757",
+        },
+      },
+    ],
     base: true,
     utils: true,
     logs: true,
     rtl: false,
     prefix: "daisy-",
   },
-}
+};
