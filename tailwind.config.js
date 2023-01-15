2a39de5f49f87b50e555351603dbad4a3634c38b
Sivert <saiverx@gmail.com>
2023-01-15 23:55:54 +0100

    Should solve no need for safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,22 +3,22 @@ module.exports = {
   corePlugins: {
     preflight: false, // disable Tailwind's reset
   },
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "../docs/**/*.mdx"], // my markdown stuff is in ../docs, not /src
-  darkMode: ["class", '[data-theme="dark"]'], // hooks into docusaurus' dark mode settigns
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./docs/**/*.{mdx,md}"],
+  darkMode: ["class", '[data-theme="dark"]'], // hooks into docusaurus' dark mode settings
   theme: {
     extend: {},
   },
 
-  safelist: [
-    { pattern: /bg-./ },
-    { pattern: /daisy-./ },
-    { pattern: /text-./ },
-    { pattern: /border-./ },
-    { pattern: /w-./ },
-    { pattern: /h-./ },
-    { pattern: /rounded-./ },
-    "rounded",
-  ],
+  // safelist: [
+  //   { pattern: /bg-./ },
+  //   { pattern: /daisy-./ },
+  //   { pattern: /text-./ },
+  //   { pattern: /border-./ },
+  //   { pattern: /w-./ },
+  //   { pattern: /h-./ },
+  //   { pattern: /rounded-./ },
+  //   "rounded",
+  // ],
 
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
 
