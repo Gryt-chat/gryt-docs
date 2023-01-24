d940391ce67a84381b9e92e77f448c7cdcf3f031
Sivert <sivertgh@stud.ntnu.no>
2023-01-24 22:39:50 +0100

    Change back to dracula

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,21 +25,7 @@ module.exports = {
   // daisyUI config (optional)
   daisyui: {
     styled: true,
-    themes: [
-      {
-        grytApp: {
-          primary: "#e6cd19",
-          secondary: "#D926AA",
-          accent: "#1FB2A5",
-          neutral: "#191D24",
-          "base-100": "#2A303C",
-          info: "#3abff8",
-          success: "#36d399",
-          warning: "#fbbd23",
-          error: "#f87272",
-        },
-      },
-    ],
+    themes: ['dracula'],
     base: true,
     utils: true,
     logs: true,
