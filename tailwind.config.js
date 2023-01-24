74ac34eac20acf679f54d78e3241d8b5dc813147
Sivert <sivertgh@stud.ntnu.no>
2023-01-24 22:55:11 +0100

    new theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,7 +25,21 @@ module.exports = {
   // daisyUI config (optional)
   daisyui: {
     styled: true,
-    themes: ['dracula'],
+    themes: [
+      {
+        grytApp: {
+          primary: "#948df5",
+          secondary: "#f5948d",
+          accent: "#8dbaf5",
+          neutral: "#191D24",
+          "base-100": "#2A303C",
+          info: "#3abff8",
+          success: "#36d399",
+          warning: "#fbbd23",
+          error: "#f87272",
+        },
+      },
+    ],
     base: true,
     utils: true,
     logs: true,
